e31455f404033851a8aac4acf89962f83110b467
elliot <elliotlofi@gmail.com>
2023-11-22 10:46:17 +0000

    File selector logic

---
+++ auto_scheduler/file_select.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import random
+
+def file_selector():
+   parent_folder = 'C:\\Users\\bluch\\Desktop\\youtube auto scheduler\\youtube_auto_scheduler\\video_content'
+   sorted_folder = os.path.join(parent_folder, 'sorted_content')
+   used_folder = os.path.join(parent_folder, 'used_content')
+
+   # Get a list of all video files in the sorted_content folder
+   videos = os.listdir(sorted_folder)
+
+   # Randomly select a video
+   video = random.choice(videos)
+
+   # Construct the full paths for the source and destination
+   source = os.path.join(sorted_folder, video)
+   destination = os.path.join(used_folder, video)
+
+   # Move the video file from the sorted_content folder to the used_content folder
+   shutil.move(source, destination)
+
+   # Return the path of the moved video
+   return destination
--- auto_scheduler/upload.py
+++ auto_scheduler/upload.py
@@ -1,12 +1,16 @@
 import os
+import time
 
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
+from googleapiclient.errors import HttpError
+
 from googleapiclient.http import MediaFileUpload
 
 from authenticate import credential_check
+from file_select import file_selector
 
 #scopes = ["https://www.googleapis.com/auth/youtube.upload"] - Upload Scope
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
@@ -19,7 +23,31 @@ def upload_short(youtube):
         regionCode="GB"
     )
     response = request.execute()
-    print(response)
+
+"""""
+def upload_short(youtube):
+   while True:
+       try:
+           request = youtube.videos().insert(
+               body={
+                  "snippet": {
+                      "title": "",
+                      "tags": [],
+                      "description": ""
+                  }
+               },
+               media_body=MediaFileUpload(file_selector())
+           )
+           response = request.execute()
+           print(response)
+           break # If no error, break the loop
+       except HttpError as e:
+           if e.resp.status == 500:
+               print('HTTP 500 error, retrying...')
+               time.sleep(10) # Wait for 10 seconds before retrying
+           else:
+               raise # If it's a different error, re-raise it#
+"""
 
 if __name__ == "__main__":
     youtube = credential_check()
