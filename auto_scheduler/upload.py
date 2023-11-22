bb9759534851d6665c21c8b0eaf22f6fb50ae40d
elliot <elliotlofi@gmail.com>
2023-11-22 19:16:53 +0000

    Improved Error Handling
    Unsure if works because I ran out of API queries

--- auto_scheduler/upload.py
+++ auto_scheduler/upload.py
@@ -2,16 +2,13 @@ import os
 import time
 import shutil
 
-from google.oauth2.credentials import Credentials
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
-
 from googleapiclient.errors import HttpError
 
 from googleapiclient.http import MediaFileUpload
 
 from google.cloud import error_reporting
 
+from authenticate import credential_check
 from file_select import file_selector
 from select_snippet import upload_random_video
 
@@ -23,14 +20,12 @@ def upload_short(youtube):
 
     description = title
 
-    file_location, video = file_selector()
-
-    source = os.path.join('C:\\Users\\bluch\\Desktop\\youtube auto scheduler\\youtube_auto_scheduler\\video_content\\used_content', video)
-    destination = os.path.join('C:\\Users\\bluch\\Desktop\\youtube auto scheduler\\youtube_auto_scheduler\\video_content\\sorted_content', video)
+    e500 = 10
+    e400 = 600
 
     while True:
-        client = error_reporting.Client(project='youtube-auto-scheduler')
         try:
+            file_location, video = file_selector()
             request = youtube.videos().insert(
                 part='snippet',
                 body={
@@ -46,18 +41,32 @@ def upload_short(youtube):
             print(response)
             break # If no error, break the loop
         except HttpError as e:
-            if e.resp.status == 500:
-                print('HTTP 500 error, retrying...')
-                client.report_exception()
-                shutil.move(source, destination)
-                time.sleep(10) # Wait for 10 seconds before retrying
-            elif e.resp.status == 400:
-                print('HTTP 400 error, retrying...')
-                client.report_exception()
-                shutil.move(source, destination)
-                time.sleep(10) # Wait for 10 seconds before retrying
+            if e.resp.status >= 500:
+                print(f'{e.resp.status} Server Error, retrying in 10 seconds...')
+                HTTP_error(video, e500)
+                
+            elif e.resp.status >= 400 & e.resp.status <= 499:
+                print(f'{e.resp.status} Client Error, retrying in 10 minutes...')
+                HTTP_error(video, e400)
+
+            elif e.resp.status >= 300 & e.resp.status <= 399:
+                print(f'{e.resp.status} Redirect Response, retrying in 10 seconds...')
+                HTTP_error(video, e500)
+
             else:
                 print('Unforseen Error')
-                client.report_exception()
-                shutil.move(source, destination)
-                raise # If it's a different error, re-raise it#
+                HTTP_error(video, e500)
+                break
+
+def HTTP_error(video, delay):
+    
+    client = error_reporting.Client(project='youtube-auto-scheduler')
+    client.report_exception()
+
+    source = os.path.join('C:\\Users\\bluch\\Desktop\\youtube auto scheduler\\youtube_auto_scheduler\\video_content\\used_content', video)
+    destination = os.path.join('C:\\Users\\bluch\\Desktop\\youtube auto scheduler\\youtube_auto_scheduler\\video_content\\sorted_content', video)
+    shutil.move(source, destination)
+    time.sleep(delay)
+
+youtube_obj = credential_check()
+upload_short(youtube_obj)
